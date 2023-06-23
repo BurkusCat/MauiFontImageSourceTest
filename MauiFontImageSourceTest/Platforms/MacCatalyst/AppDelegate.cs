c9c619a81aceb01d2ab14e186985cfd7ebdd0e2e
Jeremy Powell <jeremy@visionaid.com>
2023-06-23 12:57:44 +1200

    Add project files.

---
+++ MauiFontImageSourceTest/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFontImageSourceTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
